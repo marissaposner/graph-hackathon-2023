89254dc5761f68837ec1e9a21c6cee1f70c5a5e7
gosuto-inzasheru <mail@gosuto.ai>
2023-02-25 13:57:58 -0700

    feat: query both hosted and non-hosted subgraphs

--- poc.py
+++ poc.py
@@ -1,29 +1,60 @@
+import os
 import requests
 
 import pandas as pd
+from dotenv import load_dotenv
 
 
-query = """
-query {
-  tokenContracts(orderBy: numOwners, orderDirection: desc, first: 10) {
-    id
-    name
-    numTokens
-    numOwners
-    supportsEIP721Metadata
-  }
-}
-"""
+load_dotenv()  # take environment variables from .env.
+api_key = os.getenv("THEGRAPH_API_KEY")
+
+
+def query_thegraph(subgraph_id, query, table_name, hosted=True):
+    if hosted:
+        base_url = "https://api.thegraph.com/subgraphs/name/"
+    else:
+        base_url = f"https://gateway.thegraph.com/api/{api_key}/subgraphs/id/"
+    query_url = f"{base_url}{subgraph_id}"
+    r = requests.post(query_url, json={"query": query})
+    r.raise_for_status()
+    return r.json()["data"][table_name]
 
 
 def main():
-    subgraph = "https://api.thegraph.com/subgraphs/name/wighawag/eip721-subgraph"
+    # eg https://thegraph.com/hosted-service/subgraph/messari/erc20-holders-2022
+    json_result = query_thegraph(
+        "messari/erc20-holders-2022",
+        """
+            query {
+                tokens(first: 5) {
+                    id
+                    name
+                    symbol
+                    decimals
+                }
+            }
+        """,
+        "tokens",
+    )
+    print(pd.DataFrame(json_result))
 
-    r = requests.post(subgraph, json={"query": query})
-    r.raise_for_status()
-    print(r.json())
-    df = pd.DataFrame(r.json()["data"]["tokenContracts"])
-    print(df)
+    # eg https://thegraph.com/explorer/subgraphs/G1F2huam7aLSd2JYjxnofXmqkQjT5K2fRjdfapwiik9c?view=Overview&chain=mainnet
+    json_result = query_thegraph(
+        "G1F2huam7aLSd2JYjxnofXmqkQjT5K2fRjdfapwiik9c",
+        """
+            query {
+                marketplaces(first: 5) {
+                    id
+                    name
+                    slug
+                    network
+                }
+            }
+        """,
+        "marketplaces",
+        hosted=False,
+    )
+    print(pd.DataFrame(json_result))
 
 
 if __name__ == "__main__":
