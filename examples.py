337f1d2921da1bbb5e983c04323ef0bd01b1b677
marissaposner <marissaposner14@gmail.com>
2023-02-25 17:15:57 -0500

    improving example for decentralized hosting

--- examples.py
+++ examples.py
@@ -4,41 +4,44 @@ from thegraph import query_thegraph
 
 
 def main():
-    # eg https://thegraph.com/hosted-service/subgraph/messari/erc20-holders-2022
-    json_result = query_thegraph(
-        "messari/erc20-holders-2022",
-        """
-            query {
-                tokens(orderBy: transferCount, first: 5, orderDirection: desc) {
-                    id
-                    name
-                    symbol
-                    decimals
-                    transferCount
-                }
-            }
-        """,
-        "tokens",
-    )
-    print(pd.DataFrame(json_result))
-
-    # # eg https://thegraph.com/explorer/subgraphs/G1F2huam7aLSd2JYjxnofXmqkQjT5K2fRjdfapwiik9c?view=Overview&chain=mainnet
+    # # eg https://thegraph.com/hosted-service/subgraph/messari/erc20-holders-2022
     # json_result = query_thegraph(
-    #     "G1F2huam7aLSd2JYjxnofXmqkQjT5K2fRjdfapwiik9c",
+    #     "messari/erc20-holders-2022",
     #     """
     #         query {
-    #             marketplaces(first: 5) {
+    #             tokens(orderBy: transferCount, first: 5, orderDirection: desc) {
     #                 id
     #                 name
-    #                 slug
-    #                 network
+    #                 symbol
+    #                 decimals
+    #                 transferCount
     #             }
     #         }
     #     """,
-    #     "marketplaces",
-    #     hosted=False,
+    #     "tokens",
     # )
     # print(pd.DataFrame(json_result))
+
+    # # eg https://thegraph.com/explorer/subgraphs/G1F2huam7aLSd2JYjxnofXmqkQjT5K2fRjdfapwiik9c?view=Overview&chain=mainnet
+    json_result = query_thegraph(
+        "AwoxEZbiWLvv6e3QdvdMZw4WDURdGbvPfHmZRc8Dpfz9",
+        """
+            query {
+                CollectionDailySnapshot(
+                    where: {token1: "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"},
+                    orderBy: dailyTradedItemCount,
+                    orderDirection: desc,
+                    first: 1,
+                ) {
+                    dailyTradedItemCount
+                    date
+                }
+            }
+        """,
+        "CollectionDailySnapshot",
+        hosted=False,
+    )
+    print(pd.DataFrame(json_result))
 
 
 if __name__ == "__main__":
