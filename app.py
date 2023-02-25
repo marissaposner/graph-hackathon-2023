68e1ae6b3ba28b2e0d90bce1917cad62b9e28535
gosuto-inzasheru <mail@gosuto.ai>
2023-02-25 14:28:57 -0700

    feat: query a subgraph from the app

--- app.py
+++ app.py
@@ -3,17 +3,33 @@ import os
 import openai
 from flask import Flask, jsonify
 
+from thegraph import query_thegraph
+
+
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
-@app.route('/get-Eip721-subgraph-info', methods=['POST', 'GET'])
+@app.route("/get-thegraph-results", methods=["POST", "GET"])
 def eip_subgraph_info():
-    # data = request.get_json()
-    data = {"hello": "world"}
+    data = query_thegraph(
+        "messari/erc20-holders-2022",
+        """
+            query {
+                tokens(orderBy: transferCount, first: 5, orderDirection: desc) {
+                    id
+                    name
+                    symbol
+                    decimals
+                    transferCount
+                }
+            }
+        """,
+        "tokens",
+    )
     return jsonify({"Results": data})
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return "Hello World"
