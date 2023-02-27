0ffb7bea3fe42fde18ff126e4bd98917107d8fb6
marissaposner <marissaposner14@gmail.com>
2023-02-27 11:16:21 -0500

    datetime

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
 import openai
+import datetime as dt
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
 from helpers.thegraph import query_thegraph
@@ -18,6 +19,7 @@ def eip_subgraph_info():
     try:
         input_sentence = request.get_json()["input"]
     except:
+        # if there is no input sentance and we are just testing
         input_sentence = "find the date that the most NFTs in the otherside collection (0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258) were traded?"
     print("==========user response:==========\n", input_sentence)
     response = openai.Completion.create(
@@ -35,6 +37,7 @@ def eip_subgraph_info():
         hosted=False,
     )
     print("==========the graph response:==========\n", data)
+    print(type(data))
     return jsonify(data)
 
 
