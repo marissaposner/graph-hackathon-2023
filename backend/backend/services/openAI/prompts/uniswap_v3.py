3943ece345eb857821c3bdd44eb0901e4024db29
gosuto-inzasheru <mail@gosuto.ai>
2023-03-04 15:34:47 -0700

    feat: working uniswapv3 example

--- backend/backend/services/openAI/prompts/uniswap_v3.py
+++ backend/backend/services/openAI/prompts/uniswap_v3.py
@@ -2,30 +2,19 @@ from backend.services.openAI.graph_prompt import GraphPromptBase as Prompt
 
 
 EXAMPLES = [
-
-Prompt(
-q="How much trading volume was there on June 1, 2022?",
-o="""query {
-  uniswapDayDatas(where: { date: 1654041600 }) {
-    volumeUSD
-   }
-  }"""),
-Prompt(
-q="How much trading volume was there on June 1, 2022?",
-o="""query {
-  start: factory(
-    id: "0x1F98431c8aD98523631AE4a59f267346ea31F984"
-    block: { number: 14881677 }
-  ) {
-    totalVolumeUSD
-  }
-  end: factory(
-    id: "0x1F98431c8aD98523631AE4a59f267346ea31F984"
-    block: { number: 14887796 }
-  ) {
-    totalVolumeUSD
-  }
-}"""),
-
-
+    Prompt(
+        q="What are the top 5 USDC pools by volume?",
+        o="""{
+          liquidityPools(
+            first: 5
+            orderBy: cumulativeVolumeUSD
+            orderDirection: desc
+            where: {inputTokens_: {symbol: "USDC"}}
+          ) {
+            name
+            cumulativeVolumeUSD
+          }
+        }
+        """,
+    ),
 ]
